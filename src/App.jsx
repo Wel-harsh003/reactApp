e161927d026b0bce8545e76a859c21bb5b31adea
Wel-harsh003 <harshkumar@yes-bm.com>
2026-02-25 10:50:58 +0530

    Input field is added in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ const App = () => {
       <h1>React app</h1>
       <form action="submit">
         <input type="text" placeholder='Enter your name'/>
+        <input type="number" placeholder='Enter your age'/>
+        <input type="number" placeholder='Enter your phone'/>
+        <input type="date" placeholder='Enter your DOB'/>
         <button>Click</button>
       </form>
     </div>
