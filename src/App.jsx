2b49a6cb62c03e2465fdf87f7a54ee2ebc9bc271
Wel-harsh003 <harshkumar@yes-bm.com>
2026-02-25 11:22:36 +0530

    Revert is performed

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ const App = () => {
       <h1>React app</h1>
       <form action="submit">
         <input type="text" placeholder='Enter your name'/>
-        <input type="number" placeholder='Enter your age'/>
-        <input type="number" placeholder='Enter your phone'/>
-        <input type="date" placeholder='Enter your DOB'/>
         <button>Click</button>
       </form>
     </div>
